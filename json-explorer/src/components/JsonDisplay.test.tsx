8e28179a0982af17fc6f10590a0d649e61775553
Simon Schötz <simonsch.tz@gmail.com>
2024-08-15 11:58:08 +0200

    change test to a more meaningful testing of the basic funtionality

--- json-explorer/src/components/JsonDisplay.test.tsx
+++ json-explorer/src/components/JsonDisplay.test.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import JsonDisplay from './JsonDisplay';
 describe('Json Display Component', () => {
-  it('should render', () => {
-    render(<JsonDisplay />);
-    const header = screen.getByText(/JsonDisplay/i);
+  it('should render display the input data', () => {
+    const testObj = { test: 'json' };
+    console.log('first', JSON.stringify(testObj));
 
-    expect(header).toBeInTheDocument();
+    render(<JsonDisplay data={testObj} />);
+    const key = screen.getByText(/test/i);
+    const value = screen.getByText(/json/i);
+
+    expect(key).toBeInTheDocument();
+    expect(value).toBeInTheDocument();
   });
 });
